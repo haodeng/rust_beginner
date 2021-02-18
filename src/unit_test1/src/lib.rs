b5ef62ed86a569dd5a32a7941592d0d30945fa91
hao <hao.deng.m@gmail.com>
2021-02-18 23:28:48 +0100

    Update unit_test1

--- src/unit_test1/src/lib.rs
+++ src/unit_test1/src/lib.rs
@@ -19,6 +19,36 @@ pub fn greeting(name: &str) -> String {
     format!("Hello {}!", name)
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn new2(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,5 +109,29 @@ mod tests {
             "Greeting did not contain name, value was `{}`",
             result
         );
+    }
+
+    // Testing that a condition will cause a panic!
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    // Testing that a condition will cause a panic! with a particular panic message
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100_expected_error() {
+        Guess::new2(200);
+    }
+
+    // We can also write tests that use Result<T, E>
+    #[test]
+    fn it_works2() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
