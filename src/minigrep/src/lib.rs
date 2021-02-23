115f9ac4dc6349b51f52ae81f70199af1c79aab3
hao <hao.deng.m@gmail.com>
2021-02-23 21:14:26 +0100

    Add search to minigrep lib.rs

--- src/minigrep/src/lib.rs
+++ src/minigrep/src/lib.rs
@@ -38,7 +38,41 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? will return the error value from the current function for the caller to handle.
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+// lifetime 'a used with the contents argument and the return value.
+// the lifetime parameters specify which argument lifetime is connected to the lifetime of the return value.
+// In this case, we indicate that the returned vector should contain string slices that reference slices of the argument contents (rather than the argument query).
+// n other words, we tell Rust that the data returned by the search function will live as long as the data passed into the search function in the contents argument.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
