75708eb014fd6c4c65b81aa95fad4e9ff6e313a5
hao <hao.deng.m@gmail.com>
2021-03-06 14:11:54 +0100

    Update src/iterators.rs

--- src/iterators.rs
+++ src/iterators.rs
@@ -3,6 +3,12 @@ fn main() {
     iterator_next();
     iterator_sum();
     produce_other_iterator();
+
+    test_filters_by_size();
+
+    calling_next_counter();
+
+    using_other_iterator_trait_methods();
 }
 
 fn loop_iterator() {
@@ -51,4 +57,102 @@ fn produce_other_iterator() {
     let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
 
     assert_eq!(v2, vec![2, 3, 4]);
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+// takes ownership of a vector of shoes and a shoe size as parameters.
+// It returns a vector containing only shoes of the specified size.
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+
+    // call into_iter to create an iterator that takes ownership of the vector.
+    // Then we call filter to adapt that iterator into a new iterator that only contains elements for which the closure returns true.
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+fn test_filters_by_size() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot")
+            },
+        ]
+    );
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+// Implementing the Iterator trait on our Counter struct
+impl Iterator for Counter {
+    // set the associated Item type for our iterator to u32, meaning the iterator will return u32 values.
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+// Test
+fn calling_next_counter() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
+
+// take the values produced by an instance of Counter,
+// pair them with values produced by another Counter instance after skipping the first value,
+// multiply each pair together, keep only those results that are divisible by 3,
+// and add all the resulting values together
+fn using_other_iterator_trait_methods() {
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
 }
